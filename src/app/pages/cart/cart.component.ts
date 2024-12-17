3c7bb44b430feb3f1dbd23f14ea34676d7ec5941
itsumarashraf <itsumarashraf@gmail.com>
2024-12-18 01:39:17 +0530

    wrap

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
  cartService = inject(CartService)
+ router = inject(Router)
+
+ //computed signal
+total = computed(()=>{
+  return this.cartService.cart().reduce((p,c)=> p + c.price,0)
+ })
+ 
+
+ handleCheckout(){
+  alert('Order was placed successfully!')
+  this.cartService.cart.set([])
+  this.router.navigate(['/'])
+ }
 }
