4570ad977fdfe5f90bd5e7878b27cbc56bfeacee
itsumarashraf <itsumarashraf@gmail.com>
2024-12-31 12:30:16 +0530

    Angular forms, validation, custom validation, cross validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './pages/products-list/products-list.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+import { FormValidationComponent } from './pages/form-validation/form-validation.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +14,17 @@ export const routes: Routes = [
         path:'cart',
         pathMatch:'full',
         component:CartComponent
-    }
+    },
+    {
+        path:'add-product',
+        pathMatch:'full',
+        title:'Angular Forms',
+        component:AddProductComponent
+    },
+    {
+        path:'form-validation',
+        pathMatch:'full',
+        title:'Angular Forms validation',
+        component:FormValidationComponent
+    },
 ];
